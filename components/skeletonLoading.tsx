637b404cdcd4841bf2808d9a7389f56e1a913d9f
Brian Bat-Enkh <shijiree_mng@yahoo.com>
2024-10-16 15:31:22 -0500

    add skeleton loading

--- components/skeletonLoading.tsx
+++ components/skeletonLoading.tsx
@@ -7,6 +7,7 @@ import {
     View
 } from 'react-native';
 import COLOR_MAP from '../constants/color_map';
+import SkeletonLoadingComponent from 'react-native-skeleton-loading/index'
 
 type NewButtonProps = PropsWithChildren<{
     handlePress(): void;
@@ -14,7 +15,17 @@ type NewButtonProps = PropsWithChildren<{
 
 function SkeletonLoading(): JSX.Element {
     return (
-        <View><Text style={styles.textStyle}>Loading....</Text></View>
+        <SkeletonLoadingComponent background={"#adadad"} highlight={"#ffffff"}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <View style={{ width: 100, height: 100, backgroundColor: "#adadad", borderRadius: 10 }} />
+
+                <View style={{ flex: 1, marginLeft: 10 }}>
+                    <View style={{ backgroundColor: "#adadad", width: "50%", height: 10, marginBottom: 3, borderRadius: 5 }} />
+                    <View style={{ backgroundColor: "#adadad", width: '20%', height: 8, borderRadius: 5 }} />
+                    <View style={{ backgroundColor: "#adadad", width: '15%', height: 8, borderRadius: 5, marginTop: 3 }} />
+                </View>
+            </View>
+        </SkeletonLoadingComponent>
     );
 }
 
