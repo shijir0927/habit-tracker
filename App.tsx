3ad518367c558efebf6a5680c0fd06fd5f17ddb6
Brian Bat-Enkh <shijiree_mng@yahoo.com>
2024-10-21 19:06:30 -0500

    remove SignInScreen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomeScreen, DayScreen, HabitsScreen, SignInScreen } from './screens';
+import { HomeScreen, DayScreen, HabitsScreen } from './screens';
 
 const HomeStack = createNativeStackNavigator();
 
--- screens/index.tsx
+++ screens/index.tsx
@@ -1,7 +1,6 @@
 import HomeScreen from "./homeScreen";
 import DayScreen from "./dayScreen";
 import HabitsScreen from "./habitsScreen";
-import SignInScreen from "./signInScreen";
 import SettingsScreen from "./settingScreen";
 
-export { HomeScreen, DayScreen, HabitsScreen, SignInScreen, SettingsScreen };
+export { HomeScreen, DayScreen, HabitsScreen, SettingsScreen };
